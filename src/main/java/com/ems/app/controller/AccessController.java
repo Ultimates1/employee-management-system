306eeffc3d2bafcb53a332521b308baea6fe2214
minguyen <Mi.T.Nguyen@jpl.nasa.gov>
2018-09-17 19:12:05 -0700

    Add API to update password of a username

--- src/main/java/com/ems/app/controller/AccessController.java
+++ src/main/java/com/ems/app/controller/AccessController.java
@@ -3,12 +3,33 @@ package com.ems.app.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class AccessController {
+	
+	private String username = "abc";
+	private String password = "123";
 
 	@RequestMapping(value="/ems/access/statuscheck", method=RequestMethod.GET)
 	String statusCheck() {
 		return "<center><h1><font color=#28B463>OK: System is running</font></h1></center>";
+	}
+	
+	@RequestMapping(value="/ems/access/updatepassword/{user}", method=RequestMethod.GET)
+	String updatePassword(
+			@PathVariable("user") String user,
+			@RequestParam("oldpass") String oldPass,
+			@RequestParam("newpass") String newPass) {
+		if (!user.equals(username)) {
+			return "<center><h1><font color=#28B463>Unrecognized user " + user + "</font></h1></center>";
+		}
+		if (!oldPass.equals(password)) {
+			return "<center><h1><font color=#28B463>Unrecognized password " + oldPass + "</font></h1></center>";
+		}
+		password = newPass;
+		return "<center><h1><font color=#28B463>Password has been changed to " + password + "</font></h1></center>";
+		
 	}
 }
