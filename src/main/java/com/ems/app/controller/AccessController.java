5fab062bd233a7ec200ef470fabb71848c9b9494
Ralf <meguerdijian@cpp.edu>
2018-09-18 13:14:08 -0700

    login system

--- src/main/java/com/ems/app/controller/AccessController.java
+++ src/main/java/com/ems/app/controller/AccessController.java
@@ -3,12 +3,18 @@ package com.ems.app.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ems.app.utils.EmailSendingUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class AccessController {
-	
+
 	private String username = "abc";
 	private String password = "123";
 
@@ -16,7 +22,7 @@ public class AccessController {
 	String statusCheck() {
 		return "<center><h1><font color=#28B463>OK: System is running</font></h1></center>";
 	}
-	
+
 	@RequestMapping(value="/ems/access/updatepassword/{user}", method=RequestMethod.GET)
 	String updatePassword(
 			@PathVariable("user") String user,
@@ -30,6 +36,23 @@ public class AccessController {
 		}
 		password = newPass;
 		return "<center><h1><font color=#28B463>Password has been changed to " + password + "</font></h1></center>";
-		
+	}
+
+	@RequestMapping(value="/ems/access/login", method=RequestMethod.GET)
+	String login(@RequestParam("user") String user,
+			@RequestParam("password") String password) {
+
+		Map loginMap = new HashMap<String, String>();
+		loginMap.put("rafi", "abc");
+		loginMap.put("bhargav", "xyz");
+		loginMap.put("mi", "abc");
+		loginMap.put("darsh", "abc");
+
+		if(null != user && loginMap.containsKey(user)) {
+			if(loginMap.get(user).equals(password)) {
+				return "Login Successfully";
+			}
+		}
+		return "User id or password is wrong";
 	}
 }
