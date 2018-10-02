c82df0c9c1ee80441a435e823fb2d4dbb3695002
Ralf <meguerdijian@cpp.edu>
2018-10-02 13:23:41 -0700

    Update

--- src/main/resources/webapp/src/ui/routes/home.controller.js
+++ src/main/resources/webapp/src/ui/routes/home.controller.js
@@ -1,34 +1,40 @@
+import $ from 'jquery';
 var angular = require('angular');
-
 
 //-------------------------
 // Home controller
 //-------------------------
 angular
-	.module('ems')
-	.controller('HomeController', [
-		'$scope',
-		'$http',
-		function ($scope, $http) {
-			$scope.htmlReady = false;
-			$scope.user, $scope.oldPass, $scope.newPass, $scope.response;
+    .module('ems')
+    .controller('HomeController', [
+        '$scope',
+        '$http',
+        function ($scope, $http) {
+            $scope.htmlReady = false;
+            $scope.user, $scope.oldPass, $scope.newPass, $scope.response;
 
-			$scope.ready = function () {
-				return $scope.htmlReady;
-			};
+            $scope.ready = function () {
+                return $scope.htmlReady;
+            };
 
-			// Fired when html ready
-			$scope.htmlLoaded = function () {
-				$scope.htmlReady = true;
-			};
+            // Fired when html ready
+            $scope.htmlLoaded = function () {
+                $scope.htmlReady = true;
+            };
 
-			$scope.updatePassword = function () {
-				$http.get("ems/access/updatepassword/" + $scope.user + "?oldpass=" + $scope.oldPass + "&newpass=" + $scope.newPass)
-					.then(function success(response) {
-						$scope.response = response.data;
-					}, function error(err) {
-						$scope.response = err;
-					});
-			}
-		}
-	]);
+            $scope.updatePassword = function () {
+                $http.get("ems/access/updatepassword/" + $scope.user + "?oldpass=" + $scope.oldPass + "&newpass=" + $scope.newPass)
+                    .then(function success(response) {
+                        $scope.response = response.data;
+                    }, function error(err) {
+                        $scope.response = err;
+                    });
+            };
+
+            $(document).ready(function () {
+                $('#alert').click(function () {
+                    alert('Hello');
+                });
+            });
+        }
+    ]);
