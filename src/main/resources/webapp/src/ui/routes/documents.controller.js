bf627af48b85b3105915310fadb9f77c138c10e2
yamiryu <darkdragon735@yahoo.com>
2018-11-20 10:42:58 -0800

    #20 Add all pages placeholders

---
+++ src/main/resources/webapp/src/ui/routes/documents.controller.js
@@ -0,0 +1,27 @@
+var angular = require('angular');
+
+require('../data/access.service.js');
+
+angular
+	.module('ems')
+	.controller('DocumentsController', [
+		'$rootScope',
+		'$scope',
+		'Access',
+		function ($rootScope, $scope, Access) {
+			$scope.htmlReady = false;
+			$scope.accessContent = Access.getAccessContent();
+
+			$scope.ready = function () {
+				if (!Access.getLoginStatus()) {
+					$rootScope.goTo('login');
+				}
+				return $scope.htmlReady;
+			};
+
+			// Fired when html ready
+			$scope.htmlLoaded = function () {
+				$scope.htmlReady = true;
+			};
+		}
+	]);
