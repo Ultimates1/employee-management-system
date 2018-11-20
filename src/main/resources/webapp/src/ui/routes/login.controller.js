03785c2161d41418756d9e6d3e3d5475b9fd87e0
yamiryu <darkdragon735@yahoo.com>
2018-11-20 10:44:23 -0800

    #20 Update login to use POST; Update pages to use rootScope goTo function instead of location path

--- src/main/resources/webapp/src/ui/routes/login.controller.js
+++ src/main/resources/webapp/src/ui/routes/login.controller.js
@@ -1,34 +1,42 @@
-import $ from 'jquery';
 var angular = require('angular');
-//-------------------------
-// Login controller
-//-------------------------
+
+require('../data/access.service.js');
+
 angular
-    .module('ems')
-    .controller('LoginController', [
-        '$scope',
-        '$http',
-        function ($scope, $http) {
-            $scope.htmlReady = false;
-            $scope.user, $scope.Pass;
+	.module('ems')
+	.controller('LoginController', [
+		'$rootScope',
+		'$scope',
+		'$http',
+		'Access',
+		function ($rootScope, $scope, $http, Access) {
+			$scope.htmlReady = false;
+			$scope.user = '';
+			$scope.pass = '';
 
-            $scope.ready = function () {
-                return $scope.htmlReady;
-            };
+			$scope.ready = function () {
+				return $scope.htmlReady;
+			};
 
-            // Fired when html ready
-            $scope.htmlLoaded = function () {
-                $scope.htmlReady = true;
-            };
+			// Fired when html ready
+			$scope.htmlLoaded = function () {
+				$scope.htmlReady = true;
+			};
 
-            $scope.login = function () {
-                //?user= :=signify to http that 
-                $http.get("/ems/access/login/?user=" + $scope.user + "&password=" + $scope.Pass)
-                    .then(function success(response) {
-                        $scope.response = response.data;
-                    }, function error(err) {
-                        $scope.response = err;
-                    });
-            };
-        }
-    ]);
+			$scope.login = function () {
+				$http.post(
+					'/ems/access/login',
+					{
+						user: $scope.user,
+						password: $scope.pass
+					})
+					.then(function success(response) {
+						Access.setLoginStatus(response.data.success);
+						Access.setAccessContent(response.data.message);
+						$rootScope.goTo('home');
+					}, function error(response) {
+						Access.setLoginStatus(response.data.success);
+					});
+			};
+		}
+	]);
