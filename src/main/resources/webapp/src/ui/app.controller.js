8463ec2439d6867174329e9960fe124e9853e0d0
yamiryu <darkdragon735@yahoo.com>
2018-11-26 22:57:45 -0800

    #36 Move hasFunction from home to app; Show/hide navigation and banner base on current page

--- src/main/resources/webapp/src/ui/app.controller.js
+++ src/main/resources/webapp/src/ui/app.controller.js
@@ -13,6 +13,9 @@ angular
 		function ($rootScope, $scope, $window, $location, Access) {
 			$scope.htmlReady = false;
 			$scope.view = {};
+			$scope.panelHidden = true;
+			$scope.panelClass = 'hidden';
+			$scope.bannerImgPath = '';
 
 			// On resize
 			angular.element($window).bind('resize', function () {
@@ -38,6 +41,11 @@ angular
 				$location.path('/login');
 			};
 
+			$scope.togglePanel = function () {
+				$scope.panelHidden = !$scope.panelHidden;
+				$scope.panelClass = $scope.panelHidden ? 'hidden' : '';
+			}
+
 			$scope.$on('$routeChangeStart', function (event, next, current) {
 				let route = next.$$route || current.$$route;
 
@@ -46,9 +54,118 @@ angular
 					case 'ForgotController':
 					case 'ResetController':
 						$scope.view.logout = false;
+						$scope.view.banner = false;
+						$scope.view.nav = false;
 						break;
 					case 'HomeController':
 						$scope.view.logout = true;
+						$scope.view.banner = false;
+						$scope.view.nav = false;
+						break;
+					case 'ProfileController':
+						$scope.view.logout = true;
+						$scope.view.banner = true;
+						$scope.view.nav = true;
+						$scope.navs = {
+							profile: false,
+							timekeeping: $rootScope.hasFunction('timekeeping'),
+							leaveRequest: $rootScope.hasFunction('leaverequest'),
+							documents: $rootScope.hasFunction('documents'),
+							management: $rootScope.hasFunction('management'),
+							budget: $rootScope.hasFunction('budget'),
+							evaluation: $rootScope.hasFunction('evaluation')
+						};
+						$scope.bannerImgPath = 'assets/images/banner_profile.jpg';
+						break;
+					case 'TimekeepingController':
+						$scope.view.logout = true;
+						$scope.view.banner = true;
+						$scope.view.nav = true;
+						$scope.navs = {
+							profile: $rootScope.hasFunction('profile'),
+							timekeeping: false,
+							leaveRequest: $rootScope.hasFunction('leaverequest'),
+							documents: $rootScope.hasFunction('documents'),
+							management: $rootScope.hasFunction('management'),
+							budget: $rootScope.hasFunction('budget'),
+							evaluation: $rootScope.hasFunction('evaluation')
+						};
+						$scope.bannerImgPath = 'assets/images/banner_timekeeping.jpg';
+						break;
+					case 'LeaveRequestController':
+						$scope.view.logout = true;
+						$scope.view.banner = true;
+						$scope.view.nav = true;
+						$scope.navs = {
+							profile: $rootScope.hasFunction('profile'),
+							timekeeping: $rootScope.hasFunction('timekeeping'),
+							leaveRequest: false,
+							documents: $rootScope.hasFunction('documents'),
+							management: $rootScope.hasFunction('management'),
+							budget: $rootScope.hasFunction('budget'),
+							evaluation: $rootScope.hasFunction('evaluation')
+						};
+						$scope.bannerImgPath = 'assets/images/banner_leave.jpg';
+						break;
+					case 'DocumentsController':
+						$scope.view.logout = true;
+						$scope.view.banner = true;
+						$scope.view.nav = true;
+						$scope.navs = {
+							profile: $rootScope.hasFunction('profile'),
+							timekeeping: $rootScope.hasFunction('timekeeping'),
+							leaveRequest: $rootScope.hasFunction('leaverequest'),
+							documents: false,
+							management: $rootScope.hasFunction('management'),
+							budget: $rootScope.hasFunction('budget'),
+							evaluation: $rootScope.hasFunction('evaluation')
+						};
+						$scope.bannerImgPath = 'assets/images/banner_documents.jpg';
+						break;
+					case 'ManagementController':
+						$scope.view.logout = true;
+						$scope.view.banner = true;
+						$scope.view.nav = true;
+						$scope.navs = {
+							profile: $rootScope.hasFunction('profile'),
+							timekeeping: $rootScope.hasFunction('timekeeping'),
+							leaveRequest: $rootScope.hasFunction('leaverequest'),
+							documents: $rootScope.hasFunction('documents'),
+							management: false,
+							budget: $rootScope.hasFunction('budget'),
+							evaluation: $rootScope.hasFunction('evaluation')
+						};
+						$scope.bannerImgPath = 'assets/images/banner_management.jpg';
+						break;
+					case 'BudgetController':
+						$scope.view.logout = true;
+						$scope.view.banner = true;
+						$scope.view.nav = true;
+						$scope.navs = {
+							profile: $rootScope.hasFunction('profile'),
+							timekeeping: $rootScope.hasFunction('timekeeping'),
+							leaveRequest: $rootScope.hasFunction('leaverequest'),
+							documents: $rootScope.hasFunction('documents'),
+							management: $rootScope.hasFunction('management'),
+							budget: false,
+							evaluation: $rootScope.hasFunction('evaluation')
+						};
+						$scope.bannerImgPath = 'assets/images/banner_budget.jpg';
+						break;
+					case 'EvaluationController':
+						$scope.view.logout = true;
+						$scope.view.banner = true;
+						$scope.view.nav = true;
+						$scope.navs = {
+							profile: $rootScope.hasFunction('profile'),
+							timekeeping: $rootScope.hasFunction('timekeeping'),
+							leaveRequest: $rootScope.hasFunction('leaverequest'),
+							documents: $rootScope.hasFunction('documents'),
+							management: $rootScope.hasFunction('management'),
+							budget: $rootScope.hasFunction('budget'),
+							evaluation: false
+						};
+						$scope.bannerImgPath = 'assets/images/banner_evaluation.jpg';
 						break;
 					default:
 						break;
@@ -90,6 +207,10 @@ angular
 					default:
 						$location.path('/');
 				}
+			};
+
+			$rootScope.hasFunction = function (fn) {
+				return Access.getAccessContent().fns.indexOf(fn) !== -1;
 			};
 		}
 	]);
