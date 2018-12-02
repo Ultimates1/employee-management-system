97f4b9b0677ecdf0d1d8a987ca2128d63a0c0d76
yamiryu <darkdragon735@yahoo.com>
2018-12-01 21:31:25 -0800

    #45 Add UI layout and styling, front-end functions for Management page

--- src/main/resources/webapp/src/ui/routes/management.controller.js
+++ src/main/resources/webapp/src/ui/routes/management.controller.js
@@ -10,7 +10,7 @@ angular
 		'Access',
 		function ($rootScope, $scope, Access) {
 			$scope.htmlReady = false;
-			$scope.accessContent = Access.getAccessContent();
+			$scope.userID = Access.getAccessContent().userID;
 
 			$scope.ready = function () {
 				if (!Access.getLoginStatus()) {
@@ -26,5 +26,57 @@ angular
 			$scope.htmlLoaded = function () {
 				$scope.htmlReady = true;
 			};
+
+			$scope.getEmployeeList = function (userID) {
+				return {
+					'eA': 'employee A',
+					'eB': 'employee B',
+					'eC': 'employee C',
+					'eD': 'employee D',
+					'eE': 'employee E',
+					'eF': 'employee F',
+					'eG': 'employee G',
+					'eH': 'employee H',
+					'eI': 'employee I',
+					'eJ': 'employee J',
+					'eK': 'employee K',
+					'eL': 'employee L',
+					'eM': 'employee M',
+					'eN': 'employee N',
+					'eO': 'employee O',
+					'eP': 'employee P'
+				};
+			}
+
+			$scope.getProjectList = function (userID) {
+				return [
+					{
+						id: 'p1',
+						name: 'Project 1',
+						members: ['eA', 'eB', 'eC']
+					},
+					{
+						id: 'p2',
+						name: 'Project 2',
+						members: ['eD', 'eE', 'eH']
+					},
+					{
+						id: 'p3',
+						name: 'Project 3',
+						members: ['eF', 'eG', 'eI', 'eJ', 'eK', 'eL', 'eM', 'eN', 'eO', 'eP']
+					}
+				]
+			};
+
+			$scope.addProjectEmployee = function () {
+				$scope.selectedProject.members.push($scope.selectedEmployee);
+			}
+
+			$scope.removeProjectEmployee = function (index) {
+				$scope.selectedProject.members.splice(index, 1);
+			}
+
+			$scope.projectList = $scope.getProjectList($scope.userID);
+			$scope.employeeList = $scope.getEmployeeList($scope.userID);
 		}
 	]);
